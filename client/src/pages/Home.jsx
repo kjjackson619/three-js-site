38fec8a8326c948c94b8af2e29b91d3bdbb0bfe1
KJ Jackson <kjjackson619@yahoo.com>
2023-04-09 16:28:14 -0400

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import {useSnapshot} from 'valtio';
 import {headContainerAnimation, headContentAnimation, headTextAnimation, slideAnimation} from '../config/motion';
 
 import state from '../store';
+import { CustomButton } from '../components';
 
 const Home = () => {
     const snap = useSnapshot(state);
@@ -34,7 +35,10 @@ const Home = () => {
                             </p>
 
                             <CustomButton 
-                            
+                            type="filled"
+                            title="Customize It"
+                            handleClick={() => state.intro = false}
+                            customStyles="w-fit px-4 py-2.5 font-bold text-sm"
                             />
                         </motion.div>
                     </motion.div>
