d4b14d223e7f3e52bb729896686eb826371fb9bc
KJ Jackson <kjjackson619@yahoo.com>
2023-04-12 20:27:02 -0400

    active tab functionality

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -16,6 +16,33 @@ import {AIPicker, ColorPicker, CustomButton, FilePicker, Tab} from '../component
 const Customizer = () => {
   const snap = useSnapshot(state);
 
+  const [file, setFile] = useState('');
+
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+
+  const [activeEditorTab, setActiveEditorTab] = useState('');
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  })
+ 
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />
+   
+      case "filepicker":
+        return <FilePicker />
+     
+      case "aiPicker":
+        return <AIPicker />
+        
+      default:
+        return null;
+    }
+  }
+
   return (
    <AnimatePresence>
       {!snap.intro && (
@@ -31,11 +58,10 @@ const Customizer = () => {
                       <Tab 
                           key={tab.name}
                           tab={tab}
-                          handleClick={() => {
-                        
-                          }}
+                          handleClick={() => setActiveEditorTab(tab.name)}
                       />
                     ))}
+                    {generateTabContent()}
                 </div>
               </div>
           </motion.div>
